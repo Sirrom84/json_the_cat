159165f52da8f3e05e0badffd81142ee5facb48b
Rob <morrisrjc@gmail.com>
2021-01-15 05:32:54 +0000

    added mocha chai testing and refactored

--- breedFetcher.js
+++ breedFetcher.js
@@ -3,11 +3,18 @@ const request = require('request');
 const fetchBreedDescription = (breedName, callback) => {
   request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName.toLowerCase()}`, (error, response, body) => {
     const data = JSON.parse(body);
-  
     if (error) {
-      return  callback(error);
+     callback(error,null);
+      return;
+    } 
+    
+    if (data.length === 0) {
+      callback('Invalid breed', null);
+      return;
     }
-    return callback(null,data[0].description);
+    
+    callback(null,data[0].description);
+    return;
   });
 };
 
@@ -15,3 +22,4 @@ const fetchBreedDescription = (breedName, callback) => {
 
 module.exports = { fetchBreedDescription };
 
+ 
